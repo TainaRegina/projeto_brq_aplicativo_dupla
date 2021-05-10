53eb20fca45ae01ac55d09b856e3ed47c23308fa
TainaRegina <tainareginasoares@hotmail.com>
2021-05-10 16:40:04 -0300

    nova branch Taina

---
+++ app/src/main/java/com/example/plannerbeta/Teladenotas.kt
@@ -0,0 +1,11 @@
+package com.example.plannerbeta
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Teladenotas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_teladenotas)
+    }
+}
