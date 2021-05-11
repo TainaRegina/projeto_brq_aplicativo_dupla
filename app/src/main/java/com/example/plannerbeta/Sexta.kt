140ba7512e9674166017b34dab97a6a53a013938
TainaRegina <tainareginasoares@hotmail.com>
2021-05-10 23:22:40 -0300

    novas telas dia da semana

--- app/src/main/java/com/example/plannerbeta/Sexta.kt
+++ app/src/main/java/com/example/plannerbeta/Sexta.kt
@@ -3,9 +3,9 @@ package com.example.plannerbeta
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Teladenotas : AppCompatActivity() {
+class Sexta : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_teladenotas)
+        setContentView(R.layout.activity_sexta)
     }
 }
