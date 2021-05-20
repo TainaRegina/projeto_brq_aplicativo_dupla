e275dc3f40fd313d9becc6a9551133535f888ccd
Beatriz <beatriz.ocgl@gmail.com>
2021-05-20 11:30:05 -0300

    Remoção do Spinner e modificação nos layouts

--- app/src/main/java/com/example/plannerbeta/view/ActivityDetalhes.kt
+++ app/src/main/java/com/example/plannerbeta/view/ActivityDetalhes.kt
@@ -7,16 +7,14 @@ import android.widget.*
 import com.example.plannerbeta.R
 import com.example.plannerbeta.R.array.status
 
-class ActivityDetalhes: AppCompatActivity(), AdapterView.OnItemSelectedListener {
+class ActivityDetalhes: AppCompatActivity() {
 
 
     var tarefa: Tarefas? = null
     var editTextTituloDetalhes: EditText? = null
     var editTextDescricaoDetalhes: EditText? = null
     var editTextStatusDetalhes: EditText? = null
-    var textViewChoose: TextView? = null
-    var escolhaStatus = arrayOf("A realizar", "Em andamento", "Concluido", "Atrasado")
-    var spinner = findViewById<Spinner>(R.id.spinner)
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +23,7 @@ class ActivityDetalhes: AppCompatActivity(), AdapterView.OnItemSelectedListener 
 
         carregarElemento()
         atribuirValoremET()
-        carregarSpinnerEAdapter()
+
 
     }
 
@@ -33,7 +31,7 @@ class ActivityDetalhes: AppCompatActivity(), AdapterView.OnItemSelectedListener 
         editTextTituloDetalhes = findViewById(R.id.editText_detalhes_titulo)
         editTextDescricaoDetalhes = findViewById(R.id.editText_detalhes_descrição)
         editTextStatusDetalhes = findViewById(R.id.editText_statusDetalhes)
-        textViewChoose = findViewById(R.id.escolha_tv)
+
     }
 
     fun atribuirValoremET() {
@@ -46,18 +44,5 @@ class ActivityDetalhes: AppCompatActivity(), AdapterView.OnItemSelectedListener 
     }
    // SaveData.Tarefa = Tarefas(editTextDescricaoDetalhes?.text.toString(), editTextTituloDetalhes?.text.toString(), editTextStatusDetalhes?.text.toString())
 
-    fun carregarSpinnerEAdapter() {
-        spinner.onItemSelectedListener = this
-        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, escolhaStatus)
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinner.adapter = arrayAdapter
-    }
-
-    override fun onNothingSelected(parent: AdapterView<*>?) {
-    }
-
-    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        textViewChoose?.text = "Selecione" + escolhaStatus[position]
-    }
 
 }
